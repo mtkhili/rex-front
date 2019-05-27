d9a54f1e229f38e8c7c2a6722d7fc7d855c62afa
Mouhcine Tkhili <tkhilimouhcine@gmail.com>
2019-05-27 14:04:48 -0300

    enlever error js

---
+++ src/app/pages/create-event/create-event.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-create-event',
+  templateUrl: './create-event.page.html',
+  styleUrls: ['./create-event.page.scss'],
+})
+export class CreateEventPage implements OnInit {
+
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController
+    ) { }
+
+  ngOnInit() {
+  }
+
+  async sendData() {
+    const loader = await this.loadingCtrl.create({
+      duration: 2000
+    });
+
+    loader.present();
+    loader.onWillDismiss().then(async l => {
+      const toast = await this.toastCtrl.create({
+        showCloseButton: true,
+        cssClass: 'bg-profile',
+        message: 'Your Event was created',
+        duration: 3000,
+        position: 'bottom'
+      });
+
+      toast.present();
+      this.navCtrl.navigateForward('/login');
+    });
+  }
+
+}
